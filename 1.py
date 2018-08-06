eca7a4f13bbc40afe753684cd4a7d9bf6efefb68
Cliinky <42123962+Cliinky@users.noreply.github.com>
2018-08-07 11:07:12 +1200

    2nd Counter Update
    Added a 2nd counter function

--- 1.py
+++ 1.py
@@ -3,3 +3,7 @@ counter = 0
 
 while counter < 10:
     print("The counter is currently at {}".format(counter))
+
+counter2 = 0
+while counter2 <20:
+    print("The second counter is currently at {}".format(counter))
